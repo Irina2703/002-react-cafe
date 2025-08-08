112734b87656061b4b749871be5567b593138a6d
Kliucherova Irina <klucerovaira@gmail.com>
2025-08-08 23:11:07 +0200

    commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// src/main.tsx
+
+import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
+
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+<div>
+  <h1>Welcome to React</h1>
+  <p>
+    This is JSX — it looks like HTML, but it's not quite the same. It has its
+    own rules!
+  </p>
+</div>
+  ;
+
