9a6575c6b7d51081ddd73b4362b81ead4a1df07f
Kliucherova Irina <klucerovaira@gmail.com>
2025-08-09 16:21:40 +0200

    commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,52 @@
-// src/App.tsx
-
-function Product() {
-  return (
-    <div>
-      <h2>Cookies</h2>
-      <p>Price: 999 credits</p>
-    </div>
-  );
-}
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import css from './App.module.css';
+import type { Votes, VoteType } from '../../types/votes';
+import { useState } from 'react';
+import Notification from '../Notification/Notification';
 
 export default function App() {
-  return (
-    <>
-      <h1>Products</h1>
+  const initVotes: Votes = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  const [votes, setVotes] = useState<Votes>(initVotes);
 
-      <Product />
-      <Product />
-      <Product />
-    </>
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const canReset = totalVotes > 0;
+  const positiveRate =
+    totalVotes > 0 ? Math.round((votes.good / totalVotes) * 100) : 0;
+
+  const handleVote = (type: VoteType) => {
+    setVotes(prev => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes(initVotes);
+  };
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={canReset}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
   );
 }
